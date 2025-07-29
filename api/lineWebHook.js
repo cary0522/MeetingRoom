1cf1f08d6e4fa0122a66d032dd77564a465826af
cary0522 <cary0522@gmail.com>
2025-07-29 23:40:30 +0800

    新增測試api檔案

--- api/lineWebHook.js
+++ api/lineWebHook.js
@@ -1,5 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
 const axios = require('axios');
 const moment = require('moment');
 // 拿取env檔資料的套件
@@ -36,43 +34,46 @@ async function GetUserName(userId) {
 // line webhook接收訊息
 module.exports = async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).end();
-
-    const UserId = req.body.events[0].source.userId;
-    const UserName = await GetUserName(UserId);
-    if (req.body.events[0].message.text.includes('借用會議室')) {
-        const content = req.body.events[0].message.text;
-        let matchDate = content.match(/(\d{1,2}\/\d{1,2})/);
-        let Date = matchDate ? moment(matchDate[0], 'M/D').format('YYYY-MM-DD') : '';
-        // 取得開始時間
-        let matchStartTime = content.match(/(\d{1,2}:\d{2}[\-~～－])/);
-        let StartTime = matchStartTime ? moment(matchStartTime[0], 'HH:mm').format('HH:mm') : '';
-        // 取得結束時間
-        let matchEndTime = content.match(/([\-~～－]\d{1,2}:\d{2})/);
-        let EndTime = matchEndTime ? moment(matchEndTime[0], 'HH:mm').format('HH:mm') : '';
-        // 取得標題
-        let Summary = content.split(' ').slice(1).join(' ');
-        // 組合事件
-        let caseEvent = {
-            'summary': UserName + ' ' + Summary,
-            'start': {
-                'dateTime': `${Date}T${StartTime}:00+08:00`,
-                'timeZone': 'Asia/Taipei',
-            },
-            'end': {
-                'dateTime': `${Date}T${EndTime}:00+08:00`,
-                'timeZone': 'Asia/Taipei',
-            },
+    if (req.method === 'POST') {
+        console.log('Received webhook:', req.body);
+        const UserId = req.body.events[0].source.userId;
+        const UserName = await GetUserName(UserId);
+        if (req.body.events[0].message.text.includes('借用會議室')) {
+            const content = req.body.events[0].message.text;
+            let matchDate = content.match(/(\d{1,2}\/\d{1,2})/);
+            let Date = matchDate ? moment(matchDate[0], 'M/D').format('YYYY-MM-DD') : '';
+            // 取得開始時間
+            let matchStartTime = content.match(/(\d{1,2}:\d{2}[\-~～－])/);
+            let StartTime = matchStartTime ? moment(matchStartTime[0], 'HH:mm').format('HH:mm') : '';
+            // 取得結束時間
+            let matchEndTime = content.match(/([\-~～－]\d{1,2}:\d{2})/);
+            let EndTime = matchEndTime ? moment(matchEndTime[0], 'HH:mm').format('HH:mm') : '';
+            // 取得標題
+            let Summary = content.split(' ').slice(1).join(' ');
+            // 組合事件
+            let caseEvent = {
+                'summary': UserName + ' ' + Summary,
+                'start': {
+                    'dateTime': `${Date}T${StartTime}:00+08:00`,
+                    'timeZone': 'Asia/Taipei',
+                },
+                'end': {
+                    'dateTime': `${Date}T${EndTime}:00+08:00`,
+                    'timeZone': 'Asia/Taipei',
+                },
+            }
+            // 取得Service Account認證
+            const authClient = await auth.getClient();
+            const calendar = google.calendar({ version: 'v3', auth: authClient });
+            const result = await calendar.events.insert({
+                calendarId: '353ad998c6a7082490658174c42ceeacb50bd3a9d1e17689a9241168657c4848@group.calendar.google.com',
+                resource: caseEvent,
+            });
+    
+            res.status(200).json({ status: 200, url: result.htmlLink });
+        } else {
+            res.status(200).end();
         }
-        // 取得Service Account認證
-        const authClient = await auth.getClient();
-        const calendar = google.calendar({ version: 'v3', auth: authClient });
-        const result = await calendar.events.insert({
-            calendarId: '353ad998c6a7082490658174c42ceeacb50bd3a9d1e17689a9241168657c4848@group.calendar.google.com',
-            resource: caseEvent,
-        });
-
-        res.status(200).json({ status: 200, url: result.htmlLink });
-    } else {
-        res.status(200).end();
     }
+
 };
